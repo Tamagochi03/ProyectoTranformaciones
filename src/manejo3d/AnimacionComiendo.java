bb742a766401b6b426c515db9a157c940e9bf50f
Tamagochi03 <zielhafen.ipn@hotmail.com>
2015-03-02 00:23:49 -0600

    Update AnimacionComiendo.java

--- src/manejo3d/AnimacionComiendo.java
+++ src/manejo3d/AnimacionComiendo.java
@@ -9,47 +9,23 @@ package manejo3d;
  *
  * @author YareliS
  */
-import com.sun.j3d.utils.image.TextureLoader;
-import java.awt.Container;
-import javax.media.j3d.Background;
-import javax.media.j3d.BoundingSphere;
-import javax.media.j3d.ImageComponent2D;
-import javax.media.j3d.Shape3D;
-import javax.media.j3d.TransformGroup;
 
 
 public class AnimacionComiendo {
     
     Transformaciones trans;
-    Shape3D shape;
-    TextureLoader tex;
-    ImageComponent2D imagen;
-    Background background;
-    BoundingSphere bounds = new BoundingSphere();
-    TransformGroup Tuxcontent = new TransformGroup();
-    TransformGroup Tuxtrans = new TransformGroup();
-    TransformGroup grupoTrans;
+    Universo universo;
     
-    public AnimacionComiendo(TransformGroup grupoTrans) {  
-        Tuxcontent.addChild(Tuxtrans);
-        this.grupoTrans = grupoTrans;
+    public AnimacionComiendo(Universo universo) {  
+        this.universo = universo;
+        trans = new Transformaciones(this.universo); 
         
     }
     
-    public TransformGroup estadoComiendo() {
-        //trans = new Transformaciones(this.grupoTrans);
-        trans.trasladarRayman();
-        return grupoTrans;  
-    }
-    
-    public TransformGroup fondoComiendo() {
-        tex = new TextureLoader("src\\manejo3d\\recursos\\cocina.jpg", null);
-        imagen= tex.getImage();
-        background = new Background();
-        background.setImage(imagen);
-        background.setApplicationBounds(bounds);
-        Tuxcontent.addChild(background);
-        return Tuxcontent;
+    public void estadDurmiendo() {
+        
+        trans.cambiarFondo("src\\manejo3d\\recursos\\muerto.png");
+        
     }
     
     
